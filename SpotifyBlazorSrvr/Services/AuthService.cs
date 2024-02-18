5ec0a83b8358b36a3eccd96a71d8a017fd42747e
Ilya Kudryavtsev <75815095+valter0009@users.noreply.github.com>
2024-02-19 00:25:09 +0100

    Add project files.

---
+++ SpotifyBlazorSrvr/Services/AuthService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using SpotifyAPI.Web;
+
+namespace SpotifyBlazorSrvr.Services
+{
+	public class AuthService
+	{
+		public Uri _authUri;
+
+		public bool _isAuthed;
+
+		public NavigationManager navManager;
+
+		public PrivateUser _me;
+
+		public IConfiguration Configuration;
+
+
+
+
+		public AuthService(NavigationManager navManager, IConfiguration configuration)
+		{
+			this.navManager = navManager;
+			Configuration = configuration;
+		}
+
+
+		public void Login()
+		{
+			var baseUri = navManager.ToAbsoluteUri(navManager.BaseUri);
+
+			var loginRequest = new LoginRequest(baseUri, Configuration["SPOTIFY_CLIENT_ID"], LoginRequest.ResponseType.Token)
+			{
+				Scope = new[] { Scopes.PlaylistReadPrivate, Scopes.PlaylistReadCollaborative }
+			};
+			_authUri = loginRequest.ToUri();
+
+		}
+		public string AuthCallback()
+		{
+			var uri = new Uri(navManager.Uri);
+			var maxLen = Math.Min(1, uri.Fragment.Length);
+			Dictionary<string, string> fragmentParams = uri.Fragment.Substring(maxLen)?
+				.Split("&", StringSplitOptions.RemoveEmptyEntries)?
+				.Select(param => param.Split("=", StringSplitOptions.RemoveEmptyEntries))?
+				.ToDictionary(param => param[0], param => param[1]) ?? new Dictionary<string, string>();
+			_isAuthed = fragmentParams.ContainsKey("access_token");
+			return _isAuthed ? fragmentParams["access_token"] : null;
+		}
+
+
+
+
+
+	}
+}
---
+++ SpotifyBlazorSrvr/Services/SpotifyService.cs
@@ -0,0 +1,101 @@
+﻿using MetaBrainz.MusicBrainz;
+using Serilog;
+using SpotifyAPI.Web;
+namespace SpotifyBlazorSrvr.Services
+{
+
+
+
+	public class SpotifyService
+	{
+
+
+
+		private SpotifyClient? client;
+
+		public SpotifyService(AuthService authService)
+		{
+
+
+			client = new SpotifyClient(authService.AuthCallback());
+
+		}
+
+		public int GetRandomNumberRange(int min, int max)
+		{
+
+			Random random = new Random();
+			int randomNumber = random.Next(min, max);
+			return randomNumber;
+		}
+
+		public string GetRandomCharacter()
+		{
+			char[] alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZñÑαβγδεζηθικλμνξοπρστυφχψωАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ!@#$%^&*?0123456789אבגדהוזחטיכלמנסעפצקרשתابتثجحخدذرزسشصضطظعغفقكلمنهويअआइईउऊएऐओऔकखगघचछजझटठडढणतथदधनपफबभमयरलवशषसहঅআইঈউঊঋএঐওঔকখগঘঙচছজঝঞটঠডঢণতথদধনপফবভমযরলশষসহあいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン가나다라마바사아자차카타파하爱北从到额发个好见可乐吗你哦跑去人是他我想要在ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðòóôõöøùúûüýþÿЁЂЃЄЅІЇЈЉЊЋЌЎЏёђѓєѕіїјљњћќўџॐक़ख़ग़ज़ड़ढ़फ़य़ਅਆਇਈਉਊਏਐਓਔਕਖਗਘਙਚਛਜਝਞਟਠਡਢਣਤਥਦਧਨਪਫਬਭਮਯਰਲ".ToCharArray();
+
+			var randomIndex = GetRandomNumberRange(0, alpha.Length);
+
+			string randomChar = alpha[randomIndex].ToString();
+
+			return randomChar;
+		}
+
+		public string GetRandomSongTitle()
+		{
+			var q = new Query();
+
+
+			int randomOffset = GetRandomNumberRange(1, 10000);
+
+
+
+			var musicBrainSearchResult = q.FindRecordings(GetRandomCharacter(), 1000000, 0, true);
+
+			var randomSongTitle = musicBrainSearchResult.Results.ToList()[GetRandomNumberRange(1, musicBrainSearchResult.Results.Count)].Item.Title;
+
+			return randomSongTitle ?? "No song found";
+		}
+		public async Task<FullTrack> GetRandomTrackAsync()
+		{
+			try
+			{
+
+
+
+				var response = await client!.Search.Item(
+					new SearchRequest(SearchRequest.Types.Track, GetRandomSongTitle())
+					{
+						Limit = 50
+					});
+				if (response.Tracks != null && response.Tracks.Items.Count > 0)
+				{
+
+					var randomOffset = GetRandomNumberRange(0, response.Tracks.Items.Count);
+					var track = await client.Tracks.Get(response.Tracks.Items[randomOffset].Id);
+
+					var NumberofTracks = response.Tracks.Items.Count;
+
+					var trackName = track.Name;
+
+					Log.Information("Tracks returned: {NumberOfTracks} Offset number: {randomOffset}, Track: {trackName}",
+						NumberofTracks, randomOffset.ToString(), trackName);
+					return track;
+				}
+				return new FullTrack();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+		}
+
+
+		public async Task<RecommendationGenresResponse> GetGenresAsync()
+		{
+			RecommendationGenresResponse genres = await client!.Browse.GetRecommendationGenres();
+			return genres;
+		}
+
+	}
+}
